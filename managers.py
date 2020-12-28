4b1c6c2762039c7ad77f1cd6975fd5d832e424a4
Joaquín R. Montes <joaco.rm512@gmail.com>
2020-12-28 01:08:53 -0300

    minor fixes
    Co-Authored-By: Ruminini <63514002+Ruminini@users.noreply.github.com>

--- managers.py
+++ managers.py
@@ -2,11 +2,13 @@ import os
 import time
 from threading import Thread
 TEMP_EXT = ('bc', 'bc!', 'blf', 'cache', 'crdownload', 'download', 'part', 'partial', 'tmp', 'temp')
-EXTENSIONS = {'Pictures' : ('png', 'jpg', 'gif', 'jpeg', 'ai', 'bmp', 'ico', 'ps', 'psd', 'svg', 'tif', 'tiff', 'webp'),
-              'Videos' : ('.3g2','3gp','avi','flv','h264','m4v','mkv','mov','mp4','mpg','mpeg','rm','swf','vob','wmv'),
-              'Music' : ('aif','cda','mid','midi','mp3','mpa','ogg','wav','wma','wpl')}
+EXTENSIONS = {'Pictures': ('png', 'jpg', 'gif', 'jpeg', 'ai', 'bmp', 'ico', 'ps', 'psd', 'svg', 'tif', 'tiff', 'webp'),
+              'Videos': ('.3g2', '3gp', 'avi', 'flv', 'h264', 'm4v', 'mkv', 'mov',
+                         'mp4', 'mpg', 'mpeg', 'rm', 'swf', 'vob', 'wmv'),
+              'Music': ('aif', 'cda', 'mid', 'midi', 'mp3', 'mpa', 'ogg', 'wav', 'wma', 'wpl')}
 USER_PATH = os.environ['USERPROFILE']
 PATH = USER_PATH + '/Downloads'
+
 
 def make_counter():
     counter = {}
@@ -15,31 +17,37 @@ def make_counter():
     return counter
 
 
+def is_empty(counter):
+    empty = True
+    for k, v in counter.items():
+        if v > 0:
+            print(f"\r{v} {k} files found", end=' ')
+            empty = False
+    return empty
+
 
 class FileManager(Thread):
     def __init__(self, unknowns=False):
         Thread.__init__(self)
         self.stop = False
-        self.unknowns = unknowns                # Made to recognise unknown file types (or not)
+        self.unknowns = unknowns                        # Made to recognise unknown file types (or not)
 
     def run(self):
-        while not self.stop:                    # Loop this infinitely until I tell it stop
+        while not self.stop:                            # Loop this infinitely until I tell it stop
             files = os.listdir(PATH)
 
-            counter =  make_counter()
+            counter = make_counter()
 
             print("\rProgram running [S̲top]", end='')
 
             for f in files:
                 current_ext = f.split('.')[-1]
-                if current_ext not in TEMP_EXT:  # Checks if the file's last extension is temporary
+                if current_ext not in TEMP_EXT:         # Checks if the file's last extension is temporary
                     file_type = self.find_extension(f)
                     if self.unknowns or file_type != 'Unknown file':
                         counter[file_type] += 1
-                else:
-                    print(f'\r{f} is a temporary file', end='\n')
 
-            if not self.print_counter(counter):
+            if not is_empty(counter):
                 print()
 
             time.sleep(5)
@@ -54,12 +62,3 @@ class FileManager(Thread):
                 os.rename(f'{PATH}/{filename}', f'{USER_PATH}/{file_type}/{filename}')
                 return file_type
         return 'Unknown file'
-
-    @staticmethod
-    def print_counter(counter):
-        empty = True
-        for k, v in counter.items():
-            if v > 0:
-                print(f"\r{v} {k} files found", end=' ')
-                empty=False
-        return empty
