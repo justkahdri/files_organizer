14551007f5f2ae6b5563e28ed72fa8c6cf03dc33
Joaquín R. Montes <joaco.rm512@gmail.com>
2021-01-03 05:50:43 -0300

    Continuing with json preferences

--- main.py
+++ main.py
@@ -1,42 +1,57 @@
-from managers import FileManager, PATH
+from managers import FileManager
+import settings
+path = settings.load_preferences()['search-path']
 
 
 def _print_welcome():
-    print('Welcome to the Files Organizer')
-    print(f'Currently checking in {PATH}')
-    print('What would you like to do today?')
-    print('[C]hange observed folder'
-          '[S]elect filetypes to filter'
-          '[G]roup or Ungruop files'
-          '[L]og status'
-          '[Start] Observer')
-
+    print('Welcome to the Files Organizer\n'
+          f'Currently checking in {path}\n'
+          'What would you like to do today?\n'
+          '[C]hange observed folder\n'
+          '[S]elect filetypes to filter\n'
+          '[G]roup or Ungruop files\n'
+          '[L]og status\n'
+          '[Start] Observer\n'
+          '[H]elp --> Prints this message\n'
+          '[E]xit program')
 
 
 if __name__ == "__main__":
-    _print_welcome()
     robot = None
+    _print_welcome()
 
     while True:
-        try:
-            if robot is None:
-                command = input("Enter command [Start]/[S̲top]: ")
-                if command == 'e':
-                    break
-            else:
-                command = input()
+        command = input()
+        command.lower()
 
-            if command.lower() == "start":
+        if robot is None:
+            if command == 'c':
+                folder = input('Where do you want me to check?: ')
+                settings.change_focus(folder)
+            elif command == 's':
+                current = settings.load_preferences()['extensions']
+                print(f'The current extensions are: {current}')
+                chosen = input('What extensions would you want to look for? (Repeat from above): ').title()
+                settings.change_preferences('extensions', chosen.split())
+            elif command == 'g':
+                settings.group_ungroup()
+            elif command == 'l':
+                print(settings.load_preferences())
+            elif command == 'e':
+                break
+            elif command == "start":
                 print('⬇ The last modifications will appear here ⬇')
                 robot = FileManager()
                 robot.start()
-
-            elif command.lower() in ["stop", 's']:
-                print('Closing...')
-                robot.stop = True
-                robot.join()
-                robot = None
+            elif command in ['help', 'h']:
+                _print_welcome()
             else:
                 print('Unknown command, try again')
-        except AttributeError:
-            print('There is no program running')
+
+        elif command in ["stop", 's']:
+            print('Closing...')
+            robot.stop = True
+            robot.join()
+            robot = None
+        else:
+            print('Unknown command, try again')
--- settings.py
+++ settings.py
@@ -1,15 +1,50 @@
 # TODO
-# Ask user if wants to change the observed folder (Downloads by default)
 # Save used directories in path.csv
-# Ask user what file types wants to filter (All by default)
 # Ask user if wants to save the files in a separated folder
 # If thats the case, ask for folders name (automanaged by default)
 import json
+import os
+import pandas
 
 
-def _change_preferences(new_value, field='search_path'):
+def save_paths(value):
+    try:
+        df = pandas.read_csv('data/u-paths.csv')
+        df.append([value])
+    except pandas.errors.EmptyDataError:
+        df = pandas.DataFrame([value])
+    df.to_csv('data/paths.csv', index=False, header=False)
+
+
+def change_focus(folder):
+    with open('data/u-preferences.json', 'r+') as f:
+        data = json.load(f)
+        root = os.environ['USERPROFILE']
+        new_route = root + f'\\{folder}'
+        save_paths(new_route)
+
+        data['search-path'] = new_route  # <--- modify `field` value.
+        f.seek(0)  # <--- should reset file position to the beginning.
+        json.dump(data, f, indent=4)
+        f.truncate()  # remove remaining part
+
+    print(f'Now checking in {new_route}')
+
+
+def group_ungroup():
+    with open('data/u-preferences.json', 'r+') as f:
+        data = json.load(f)
+        data['group_files']['active'] = not data['group_files']['active']
+        f.seek(0)  # <--- should reset file position to the beginning.
+        json.dump(data, f, indent=4)
+        f.truncate()  # remove remaining part
+
+    print("The file's groups are now " + ('enabled' if data['group_files']['active'] else 'disabled'))
+
+
+def change_preferences(field, new_value):
     # Opening JSON file
-    with open('data/preferences.json', 'r+') as f:
+    with open('data/u-preferences.json', 'r+') as f:
         data = json.load(f)
         data[field] = new_value  # <--- modify `field` value.
         f.seek(0)  # <--- should reset file position to the beginning.
@@ -17,3 +52,9 @@ def _change_preferences(new_value, field='search_path'):
         f.truncate()  # remove remaining part
 
     print('Updated', field)
+
+
+def load_preferences():
+    with open('data/u-preferences.json', 'r') as f:
+        data = json.load(f)
+        return data
